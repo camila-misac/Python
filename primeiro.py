b7204438844946195ec4831d5da56c66dc6ab4b0
camila-misac <dev.camilamouraisac@gmail.com>
2025-02-08 21:26:48 +0000

    def soma

--- primeiro.py
+++ primeiro.py
@@ -1,5 +1,14 @@
 def saudacao(nome):
     return f"Seja bem-vindo {nome}"
 
+def soma(numero1, numero2):
+    resultado = numero1 + numero2
+    return f"A soma entre {numero1} + {numero2} = {resultado}"
+
 nome = input("Digite o seu nome: ")
-print(saudacao(nome))
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+nome = saudacao(nome)
+resultado = soma(numero1, numero2)
+print(nome)
+print(resultado)
